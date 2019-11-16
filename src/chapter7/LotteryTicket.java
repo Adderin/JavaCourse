26e9f91cc1ce8fdd59be4756f813589db8b700f8
Adderin <adderin@gmail.com>
2019-11-16 19:00:06 +0100

    Binary search and sorting added

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -11,6 +11,7 @@ public class LotteryTicket {
     public static void main(String args[]){
 
         int [] ticket = generateNumbers();
+        Arrays.sort(ticket);
         printTicket(ticket);
     }
 
@@ -31,8 +32,20 @@ public class LotteryTicket {
 
     return ticket;
     }
+
+    /**
+     * Does a sequential search on the array to find a value
+     * @param array Array to search through
+     * @param numberToSearchFor Value to search for
+     * @return true if found and false is not
+     */
+
     public static boolean search(int[] array, int numberToSearchFor) {
 
+        /* This is called an enhanced loop.
+            It iterates through 'array' and each time
+            assigns the current element to 'value'
+         */
         for(int value : array) {
             if(value == numberToSearchFor) {
                 return true;
@@ -43,6 +56,7 @@ public class LotteryTicket {
 
     public static boolean binarySearch(int[] array, int numberToSearchFor){
 
+        // To do binary - array must be sorted first
         Arrays.sort(array);
 
         int index = Arrays.binarySearch(array, numberToSearchFor);
