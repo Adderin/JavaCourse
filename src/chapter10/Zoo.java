f85ade34ab80dc24d4fc6dfab2e572003c26a4e0
Adderin <adderin@gmail.com>
2019-11-26 18:37:48 +0100

    chapter10: polymorphism

--- src/chapter10/Zoo.java
+++ src/chapter10/Zoo.java
@@ -7,5 +7,26 @@ public class Zoo {
         Dog rocky = new Dog();
         rocky.fetch();
         rocky.makeSound();
+        feed(rocky);
+
+        Animal sasha = new Dog();
+        sasha.makeSound();
+        feed(sasha);
+
+        sasha = new Cat();
+        sasha.makeSound();
+        ((Cat) sasha).scratch();
+        feed(sasha);
+    }
+
+    public static void feed(Animal animal){
+
+        if(animal instanceof Dog){
+            System.out.println("Here is your dog food!");
+        }
+        else if (animal instanceof Cat){
+            System.out.println("Here is your cat food!");
+        }
+
     }
 }
