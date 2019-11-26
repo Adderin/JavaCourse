94be7c77a3ead114a884b56d0a7250097e363ed6
Adderin <adderin@gmail.com>
2019-11-26 13:45:05 +0100

    chapter10: polymorphism explained

--- src/chapter10/Dog.java
+++ src/chapter10/Dog.java
@@ -6,4 +6,8 @@ public class Dog extends Animal {
     public void makeSound(){
         System.out.println("Who does the Woof sound?");
     }
+
+    public void fetch(){
+        System.out.println("Fetch is fun!");
+    }
 }
---
+++ src/chapter10/Zoo.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public class Zoo {
+
+    public static void main(String[] args){
+
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
+    }
+}
