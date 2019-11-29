ae1c96174da6c8185b75ec1f487006d190f649f3
Adderin <adderin@gmail.com>
2019-11-29 18:00:57 +0100

    chapter11: interfaces

---
+++ src/chapter11/Product.java
@@ -0,0 +1,13 @@
+package chapter11;
+
+public interface Product {
+
+    double getPrice();
+    void setPrice(double price);
+
+    String getName();
+    void setName();
+
+    String getColor();
+    void setColor(String color);
+}
