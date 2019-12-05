a2094ab58ea9bdedf05b3919cda38f18b6ad4d89
Adderin <adderin@gmail.com>
2019-12-05 21:41:52 +0100

    chapter13: using pipe with exceptions

--- src/chapter13/ExceptionHandling.java
+++ src/chapter13/ExceptionHandling.java
@@ -1,11 +1,30 @@
 package chapter13;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
 
-        createNewFile();
+        //createNewFile();
+        numbersExceptionHandling(); 
+    }
+
+    private static void numbersExceptionHandling() {
+        File file = new File("resources/numbers.txt");
+        try {
+            Scanner fileReader = new Scanner(file);
+
+            while(fileReader.hasNext()) {
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        } catch (FileNotFoundException | InputMismatchException e){
+            System.out.println("The file is not found");
+            e.printStackTrace();
+        }
     }
 
     public static void createNewFile(){
